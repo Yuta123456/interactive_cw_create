d481b31c04f80534c56be9a378add6f67ca5cb09
Yuta123456 <yuuta09090530@icloud.com>
2023-10-14 18:25:45 +0900

    2023-10-14

--- analyze/main.py
+++ analyze/main.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
-data = pd.read_csv('D:/M1/fashion/optimization/analyze/versatility.csv', header=None)
+data = pd.read_csv('D:/M1/fashion/optimization/analyze/random_versatility.csv', header=None)
 print(data.corr())
 print(data.describe())
+
+# print(len(data[data.iloc[:, 1] > data.iloc[:, 3]])) # 0
+# print(len(data[data.iloc[:, 1] > data.iloc[:, 5]])) # 259
+# print(len(data[data.iloc[:, 2] > data.iloc[:, 4]])) # 500
+# print(len(data[data.iloc[:, 3] > data.iloc[:, 5]])) # 500
+# print(len(data[data.iloc[:, 0] > data.iloc[:, 4]])) # 499
--- fashion_class/ImageStruct.py
+++ fashion_class/ImageStruct.py
@@ -8,17 +8,20 @@ from fashion_class.FashionItem import FashionItem
 NUM_ITEMS = 1270388
 class ImageStruct():
     def __init__(self, annotation_file, tensor_file, init_item_length = 3000):
-        selected_indices = random.sample(range(NUM_ITEMS), init_item_length)  # init_item_length個だけランダムにインデックスを選択
-        self.annotations = pd.read_csv(annotation_file).iloc[selected_indices]
-        self.img_tensors = torch.load(tensor_file)[selected_indices]
+        # selected_indices = random.sample(range(NUM_ITEMS), init_item_length)  # init_item_length個だけランダムにインデックスを選択
+        # print(selected_indices[:5])
+        # self.annotations = pd.read_csv(annotation_file).iloc[selected_indices]
+        # self.img_tensors = torch.load(tensor_file)[selected_indices]
+        self.annotations = pd.read_csv(annotation_file)
+        self.img_tensors = torch.load(tensor_file)
         self.tops: list[FashionItem] = []
         self.bottoms: list[FashionItem] = []
         self.shoes: list[FashionItem] = []
         
         for i in range(init_item_length):
-            # if i % 10 == 0:
-            #     progress = '=' * (i * 100 // init_item_length) + ' ' * (100 - (i * 100 // init_item_length))
-            #     print(f'\r【{progress}】', end='')
+            if i % 10 == 0:
+                progress = '=' * (i * 100 // init_item_length) + ' ' * (100 - (i * 100 // init_item_length))
+                print(f'\r【{progress}】', end='')
             _, item = self.get(i)
             if item.get_category() == "tops":
                 self.tops.append(item)
